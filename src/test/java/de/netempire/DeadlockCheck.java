5d68571bb44bcd15b7f14e4ec9316707e30b54d5
m_goebel <murat.goebel@googlemail.com>
2020-03-24 15:38:41 +0100

    15:40 24032020

---
+++ src/test/java/de/netempire/DeadlockCheck.java
@@ -0,0 +1,37 @@
+package de.netempire;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+public class DeadlockCheck {
+
+    @Test
+    public void startProcess() {
+        String[] result = new String[0];
+        try {
+            result = ExampleWithoutDeadlock.startProcess();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        // Detect if the java program has occurred a deadlock.
+        long[] threadIds = bean.findDeadlockedThreads();
+
+        if (threadIds != null) {
+            ThreadInfo[] infos = bean.getThreadInfo(threadIds);
+            System.out.println("Your Programm has a Deadlock.");
+            for (ThreadInfo info : infos) {
+                System.out.println(info.toString());
+            }
+        } else {
+            System.out.println("Your Programm has no Deadlock.");
+        }
+        Assert.assertNull(threadIds);
+    }
+
+}
